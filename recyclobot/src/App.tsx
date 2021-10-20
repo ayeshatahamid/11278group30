fa9a5d6b1b464fdcaa533adbd29b0cfc15fffc7c
ayeshatahamid <a.tahamid@ufl.edu>
2021-10-20 12:35:34 -0400

    adding camera functionality, App.tsx css

--- recyclobot/src/App.tsx
+++ recyclobot/src/App.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   const videoRef = useRef(null);
+   const photoRef = useRef(null);
+
+   const [hasPhoto, setHasPhoto] = useState(false);
+
+   const getVideo = () => {
+     navigator.mediaDevices
+     .getUserMedia({ 
+       video: { width: 1920, height: 1080 } 
+      })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(err);
+      })
+   }
+  
+
+   useEffect(() => {
+      getVideo();
+   }, [videoRef])
+
+
   return (
     <div className="App">
+      <div className="camera">
+        <video ref={videoRef}></video>
+          <button>Take Photo</button>
+      </div>
+      <div className={'result' + (hasPhoto ? 'hasPhoto'
+      : '')}>
+        <canvas ref={photoRef}></canvas>
+        <button> Retake Picture</button>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <code> Recyclobot </code>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        Instructions
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
